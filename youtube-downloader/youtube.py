9ced989cc549aad31017e2c3bc341beb0e093e70
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-29 21:45:43 -0300

    add function and try/except block

--- youtube-downloader/youtube.py
+++ youtube-downloader/youtube.py
@@ -1,3 +1,11 @@
 from pytube import YouTube
 from tkinter import tk
 from tkinter import filedialog
+
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+
+    except Exception as e:
+        print(e)
