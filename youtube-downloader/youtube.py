71f5edbbcc95650d5ab2c69f68b0ca17eeb6a94e
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-29 21:49:26 -0300

    add print statement

--- youtube-downloader/youtube.py
+++ youtube-downloader/youtube.py
@@ -9,6 +9,7 @@ def download_video(url, save_path):
         streams = yt.streams.filter(progressive=True, file_extension='mp4')
         highest_res = streams.get_highest_resolution()
         highest_res.download(output_path=save_path)
+        print("Downloaded successfully!")
 
     except Exception as e:
         print(e)
