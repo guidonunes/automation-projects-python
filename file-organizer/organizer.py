193ccc857c49c7b8428dd49cf87f72142839bc1d
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-29 23:06:32 -0300

    add for loop

--- file-organizer/organizer.py
+++ file-organizer/organizer.py
@@ -3,3 +3,7 @@ import shutil
 
 path = input("Enter the path of the directory to organize: ")
 files = os.listdir(path)
+
+for file in files:
+    filename, file_extension = os.path.splitext(file)
+    file_extension = file_extension[1:]
