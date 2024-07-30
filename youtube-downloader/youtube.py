0ddeca3ce590c32f7b5ef08d5d2b862cbb5e1f11
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-29 21:48:41 -0300

    add code to get highest resolution videos

--- youtube-downloader/youtube.py
+++ youtube-downloader/youtube.py
@@ -6,6 +6,9 @@ from tkinter import filedialog
 def download_video(url, save_path):
     try:
         yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension='mp4')
+        highest_res = streams.get_highest_resolution()
+        highest_res.download(output_path=save_path)
 
     except Exception as e:
         print(e)
