7af2a4a207f7b63dc02b5cec998179122a63528a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-29 23:10:50 -0300

    implement if statement

--- file-organizer/organizer.py
+++ file-organizer/organizer.py
@@ -7,3 +7,9 @@ files = os.listdir(path)
 for file in files:
     filename, file_extension = os.path.splitext(file)
     file_extension = file_extension[1:]
+
+    if os.path.exists(path + '/' + file_extension):
+        shutil.move(path + '/' + file, path + '/' + file_extension + '/' + file)
+    else:
+        os.makedirs(path + '/' + file_extension)
+        shutil.move(path + '/' + file, path + '/' + file_extension + '/' + file)
