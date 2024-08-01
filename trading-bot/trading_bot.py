b8bb937478af32968f6745cdd21102a9454d5bf5
Guilherme Nunes <guilherme.augd@gmail.com>
2024-07-31 22:56:36 -0300

    add variables to store settings

--- trading-bot/trading_bot.py
+++ trading-bot/trading_bot.py
@@ -7,3 +7,9 @@ from binance.error import ClientError
 
 
 client = UMFutures(key=api, secret=secret_key)
+
+tp = 0.01   # Take profit
+sl = 0.01   # Stop loss
+volume = 50 # Volume in USDT
+leverage = 10
+type = 'ISOLATED' # Isolated margin
